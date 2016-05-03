d6077614823b67ebcaa3b9f38c1dd2400c4c4405
shui91 <sherman.sy.hui@gmail.com>
2016-05-02 22:20:04 -0700

    feat: styled cards and going for simple v1 features before refactoring

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,82 +5,104 @@
 
 	function twitchDataService($http, $q, streamers){
 		return {
-			lookUp: function(url){
+			lookUp: function(url, type){
 				let endpoints = streamers.map(function(streamer){
-					let endpoint = url + streamer + '?callback=JSON_CALLBACK';
+					let endpoint = url + type + '/' + streamer + '?callback=JSON_CALLBACK';
+					console.log(endpoint);
 					return endpoint;
 				});
+				// let endpoints = streamers.map(function(streamer){
+				// 	let endpoint = url + streamer + '?callback=JSON_CALLBACK';
+				// 	return endpoint;
+				// });
 
 
 				return $q.all(endpoints.map(function(endpoint){
-						return $http.jsonp(endpoint).then(function(response){
-							let data = response.data,
-								streamer = {},
-								streamerData = [];
-
-							if (data.stream === null){
-								streamer.status = 'offline',
-								streamer.url = data['_links'].channel;
-								streamerData.push(streamer);
-								console.log('streamer is offline');
-							} else if (data.stream === undefined || data.error){
-								streamer.status = 'account does not exist';
-								streamerData.push(streamer);
-								console.log('streamer does not exist or account has been deleted');
-							} else if (data.stream){
-								streamer.name = data.display_name,
-								streamer.url = data['_links'].channel,
-								streamer.status = 'online',
-								streamer.logo = '',
-								streamer.excerpt = data.status;
-								streamerData.push(streamer);
-								//self.streamerInfo.push(data.stream['_links'].self);
-								//console.log('streamer is online', data.stream['_links'].self);
-							}
-							return streamerData;
-						});
+					return $http.jsonp(endpoint).then(function(response){
+						console.log(response.data);
+						let data = response.data;
+						return data;
+					});
+					// else if (endpoint.includes('channel')){
+					// 	console.log('channel data');
+					// };
+					// return $http.jsonp(endpoint).then(function(response){
+					// 	let data = response.data,
+					// 		streamer = {},
+					// 		streamerData = [];
+					// 	console.log('full returned data', data);
+					// 	if (data.stream === null){
+					// 		streamer.status = 'offline',
+					// 		streamer.url = data['_links'].channel;
+					// 		streamerData.push(streamer);
+					// 		//console.log('streamer is offline');
+					// 	} else if (data.stream === undefined || data.error){
+					// 		streamer.status = 'account does not exist';
+					// 		streamerData.push(streamer);
+					// 		//console.log('streamer does not exist or account has been deleted');
+					// 	} else {
+					// 		//this is where i should make the call to channel data bc then we have online streamers
+					// 		streamer.name = data.display_name,
+					// 		streamer.url = data['_links'].channel,
+					// 		streamer.status = 'online',
+					// 		streamer.logo = '',
+					// 		streamer.excerpt = data.status;
+					// 		//console.log('streamer object', streamer);
+					// 		streamerData.push(streamer);
+					// 		//self.streamerInfo.push(data.stream['_links'].self);
+					// 		//console.log('streamer is online', data.stream['_links'].self);
+					// 	}
+					// 	return streamerData;
+					// });
 				}));
 			}
-			// lookUp: function(){
-			// 	//go through each streamer and return appropriate url
-			// 	let ChannelUrls = streamers.map(function(streamer){
-			// 		return channelEndpoint + streamer + '?callback=JSON_CALLBACK';
-			// 	});
-			//
-			// 	let streamUrls = streamers.map(function(streamer){
-			// 		return streamUrls + streamer + '?callback=JSON_CALLBACK';
-			// 	});
-			//
-			// 	// go through each streamer and make a call, q makes a cb after all requests resolve
-			// 	return $q.all(urls.map(function(url){
-			// 		return $http.jsonp(url).then(function(res){
-			// 			let data = res.data;
-			// 			console.log(res.data);
-            //             return data;
-			// 		});
-			// 	}));
-			// }
 		};
 	}
 
 	twitchDataService.$inject = ['$http', '$q', 'streamers'];
 
-	function twitchAppCtrl($scope, $q, twitchDataService, channelEndpoint, streamEndpoint, streamers){
+	function twitchAppCtrl($scope, $q, twitchDataService, endpoint, streamers){
 		let self = $scope.tc = this;
 
 		self.streamers = streamers;
 
+		self.streamerData = [];
 
-		twitchDataService.lookUp(streamEndpoint).then(function(res){
-			console.log(res);
+		//twitchDataService.lookUp(endpoint, 'channel');
+		twitchDataService.lookUp(endpoint, 'streams').then(function(res){
 			res.forEach(function(data){
-				console.log(data);
-			});
-		}).then(function(){
-			twitchDataService.lookUp(channelEndpoint).then(function(res){
-				console.log('should have full streamer object', res);
+				self.streamer = {
+					name: null,
+					url: null,
+					status: 'offline'
+				};
+				if (data.stream === null){
+					self.streamer.name = 'streamer sleeping';
+					self.streamer.status = 'offline';
+					self.streamer.url = data['_links'].self;
+				} else if (data.stream === undefined){
+					self.streamer.name = 'streamer dead';
+					self.streamer.status = 'account does not exist';
+					self.streamer.url = 'http://i.imgur.com/nJDOS.png';
+				} else {
+					self.streamer.name = 'streamer online';
+					self.streamer.status = 'online';
+					self.streamer.url = data['_links'].self;
+				}
+				self.streamerData.push(self.streamer);
 			});
 		});
+		// twitchDataService.lookUp(streamEndpoint).then(function(res){
+		// 	console.log('stream data ', res);
+		// 	res.forEach(function(data){
+		// 		console.log(data);
+		// 	});
+		// }).then(function(){
+		// 	twitchDataService.lookUp(channelEndpoint).then(function(res){
+		// 		console.log('channel data ', res);
+		// 		//console.log('should have full streamer object', res);
+		// 	});
+		// });
 
 
         // twitchDataService.lookUp().then(function(res){
@@ -106,12 +128,12 @@
 
 	}
 
-	twitchAppCtrl.$inject = ['$scope', '$q', 'twitchDataService', 'channelEndpoint', 'streamEndpoint', 'streamers'];
+	twitchAppCtrl.$inject = ['$scope', '$q', 'twitchDataService', 'endpoint', 'streamers'];
 
 	angular
 		.module('twitchApp', [])
-		.constant('channelEndpoint', 'https://api.twitch.tv/kraken/channels/')
-		.constant('streamEndpoint', 'https://api.twitch.tv/kraken/streams/')
+		.constant('endpoint', 'https://api.twitch.tv/kraken/')
+		// .constant('streamEndpoint', 'https://api.twitch.tv/kraken/streams/')
 		.constant('streamers', ['brunofin', 'comster404', 'freecodecamp', 'storbeck', 'terakilobyte', 'habathcx', 'RobotCaleb', 'thomasballinger', 'noobs2ninjas', 'beohoff', 'test_channel', 'cretetion', 'sheevergaming', 'TR7K', 'OgamingSC2', 'ESL_SC2'])
 		.controller('twitchAppCtrl', twitchAppCtrl)
 		.service('twitchDataService', twitchDataService);
