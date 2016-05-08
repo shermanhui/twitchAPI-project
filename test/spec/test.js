49750a82acf9a9136c434ece53132b81c65135a1
shui91 <sherman.sy.hui@gmail.com>
2016-05-07 20:00:21 -0700

    feat: add simple practice test

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,10 +1,26 @@
-(function () {
+(function() {
   'use strict';
 
-  describe('Give it some context', function () {
-    describe('maybe a bit more context here', function () {
-      it('should run here few assertions', function () {
+  describe('TwitchAPI Project Tests', function() {
+    beforeEach(module('twitchApp'));
 
+    let twitchAPICtrl,
+      scope,
+      streamers,
+      endpoint;
+
+    describe('Test twitchAPICtrl', function() {
+      console.log('running test');
+      beforeEach(inject(function($controller, $rootScope, _streamers_, _endpoint_) {
+        scope = $rootScope.$new();
+        twitchAPICtrl = $controller('twitchAPICtrl', {
+          $scope: scope
+        });
+        streamers = _streamers_;
+        endpoint = _endpoint_;
+      }));
+      it('should inject constants', function() {
+        expect(streamers).toEqual(['brunofin', 'comster404', 'freecodecamp', 'storbeck', 'terakilobyte', 'habathcx', 'RobotCaleb', 'thomasballinger', 'noobs2ninjas', 'beohoff', 'test_channel', 'cretetion', 'sheevergaming', 'TR7K', 'OgamingSC2', 'ESL_SC2']);
       });
     });
   });
